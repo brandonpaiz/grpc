8c1a34946398cc6e48381724ba09167b194d3662
Brandon Paiz <brandonpaiz@google.com>
2021-09-29 17:16:06 +0000

    Use HTTPS, not SSH to access git repo

--- tools/internal_ci/linux/grpc_e2e_performance_v2.sh
+++ tools/internal_ci/linux/grpc_e2e_performance_v2.sh
@@ -64,7 +64,7 @@ go get "golang.org/dl/${TEST_INFRA_GOVERSION}"
 
 # Clone test-infra repository to one upper level directory than grpc.
 pushd ..
-git clone --recursive --single-branch --branch save-driver-logs git@github.com:brandonpaiz/test-infra
+git clone --recursive --single-branch --branch save-driver-logs https://github.com/brandonpaiz/test-infra.git
 cd test-infra
 make GOCMD="${TEST_INFRA_GOVERSION}" all-tools
 popd
