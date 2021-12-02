757b2b71e56294f7a4c3c46e1cc365a2e7e8e6a7
Brandon Paiz <brandonpaiz@google.com>
2021-12-02 01:09:42 +0000

    Adjust config for new postgres replicator layout

--- tools/run_tests/performance/dashboard_config/configure.sh
+++ tools/run_tests/performance/dashboard_config/configure.sh
@@ -2,21 +2,55 @@
 
 set -ex
 
-# Copy postgres migrator and grafana template
-git clone --branch database_migrator --depth 1 https://github.com/brandonpaiz/test-infra
-# TODO when tools merged into test-infra: git clone --depth 1 https://github.com/grpc/test-infra
-cp -r test-infra/tools/postgres_migrator .
-cp -r test-infra/tools/grafana .
+# check_env checks that all required environment variables are set
+check_env () {
+  env_vars=("$@")
+  missing_var=0
+  for var in "${env_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+      echo "${var} not set"
+      missing_var=1
+    fi
+  done
+  return $missing_var
+}
 
-# Configure database migrator
-cp postgres_migrator_config/transfer.yaml postgres_migrator/config/transfer.yaml
-cp postgres_migrator_config/Dockerfile postgres_migrator
-cp postgres_migrator_config/app.yaml postgres_migrator
+# substitute_env_in_files injects environment variables to files passed as
+# arguments
+substitute_env_in_files () {
+  files=("$@")
+  for file in "${files[@]}"; do
+    tmp=$(mktemp)
+    echo "Making substitutions in ${file}"
+    cp --attributes-only --preserve ${file} ${tmp}
+    cat ${file} | envsubst > $tmp && mv $tmp $file
+  done
+}
+
+# Check required environment variables
+check_env GCP_PROJECT_ID GCP_GRAFANA_SERVICE GCP_DATA_TRANSFER_SERVICE BQ_PROJECT_ID PG_USER PG_PASS PG_DATABASE GRAFANA_ADMIN_PASS CLOUD_SQL_INSTANCE || exit 1
+
+# Copy postgres replicator and grafana template
+git clone --branch add-postgres-replicator --depth 1 https://github.com/brandonpaiz/test-infra
+# TODO when tools merged into test-infra: git clone --depth 1 https://github.com/grpc/test-infra
+
+# Configure postgres replicator
+mkdir postgres_replicator
+mv test-infra postgres_replicator
+cp postgres_replicator_config/* postgres_replicator
 
 # Configure grafana
-cp grafana_config/app.yaml grafana
-cp -r grafana_config/provisioning grafana
-cp -r grafana_config/dashboards grafana
+#cp grafana_config/app.yaml grafana
+#cp -r grafana_config/provisioning grafana
+#cp -r grafana_config/dashboards grafana
 
-# Inject envrionment variables
-./sub_env.sh
+# Substitute environment variables
+substitute_env_in_files \
+  "./postgres_replicator/app.yaml" \
+  "./postgres_replicator/replicator_config.yaml"
+
+## Substitute environment variables
+#substitute_env_in_files \
+#  "./grafana/app.yaml" \
+#  "./postgres_replicator/app.yaml" \
+#  "./postgres_replicator/replicator_config.yaml"
